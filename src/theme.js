de3d96587bd7c195bfcceff6c3a5683d5e5213f3
rafa-almeida <rafasegt@gmail.com>
2023-06-15 21:58:40 -0300

    estrutura inicial + material.ui

---
+++ src/theme.js
@@ -0,0 +1,11 @@
+import { createTheme } from "@material-ui/core/styles";
+
+const theme = createTheme({
+    pallte: {
+        primary: {
+            main: '#000000',
+        }
+    }
+})
+
+export default theme
